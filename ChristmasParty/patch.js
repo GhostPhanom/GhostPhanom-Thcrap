dcc54535a5736aac0cf4417a55d92ccd6cd80284
GhostPhanom <34610185+GhostPhanom@users.noreply.github.com>
2017-12-17 00:31:30 +0100

    Thcrap Mods
    I sometimes create very strange stuff...

---
+++ ChristmasParty/patch.js
@@ -0,0 +1,13 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin/",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en"
+	],
+	"id": "ChristmasParty",
+	"servers": [
+		"https://raw.githubusercontent.com/GhostPhanom/GhostPhanom-Thcrap/master/ChristmasParty/"
+	],
+	"title": "Christmas in Gensokyo"
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "theghostphanom@gmail.com",
+	"id": "GhostPhanom-Thcrap",
+	"patches": {
+		"ChristmasParty": "The return of Ghosty! (English)"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/GhostPhanom/GhostPhanom-Thcrap/master/"
+	],
+	"title": "GhostPhanom's strange thoughts"
+}
