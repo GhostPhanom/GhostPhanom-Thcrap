39662bc6ab0e221a3d11d995f38a8d83b5cc7878
GhostPhanom <34610185+GhostPhanom@users.noreply.github.com>
2017-12-17 01:00:47 +0100

    update

--- ChristmasParty/patch.js
+++ ChristmasParty/patch.js
@@ -2,11 +2,13 @@
 	"dependencies": [
 		"nmlgc/base_tsa",
 		"nmlgc/script_latin/",
-		"nmlgc/western_name_order"
+		"nmlgc/western_name_order",
+		"thpatch/lang_en"
 	],
 	"id": "ChristmasParty",
 	"servers": [
 		"https://raw.githubusercontent.com/GhostPhanom/GhostPhanom-Thcrap/master/ChristmasParty/"
 	],
-	"title": "Christmas in Gensokyo"
+	"title": "Christmas in Gensokyo",
+	"update": true
 }
