6b38d9098e29f1ca8c241aefcb3c9cdd80d75e06
GhostPhanom <34610185+GhostPhanom@users.noreply.github.com>
2017-12-21 21:36:20 +0100

    Christmas

--- ChristmasParty/files.js
+++ ChristmasParty/files.js
@@ -1,5 +1,11 @@
 {
 	"patch.js": 507221831,
+	"th07/se_bonus.wav": 1114191572,
+	"th07/se_bonus2.wav": 1114191572,
+	"th07/se_cat00.wav": 2733146176,
+	"th07/se_tan00.wav": 3167541244,
+	"th07/se_tan01.wav": 3167541244,
+	"th07/se_tan02.wav": 3167541244,
 	"th07/th07logo.jpg": null,
 	"th07/title00.jpg": 2781866777
 }
