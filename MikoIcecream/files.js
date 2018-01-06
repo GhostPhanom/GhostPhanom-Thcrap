11fff489c4cd8e012d95c242441c2469c8e72112
GhostPhanom <34610185+GhostPhanom@users.noreply.github.com>
2018-01-06 21:23:11 +0100

    Miko Icecream

--- MikoIcecream/files.js
+++ MikoIcecream/files.js
@@ -1,5 +1,5 @@
 {
-	"patch.js": 3627934135,
+	"patch.js": 1538035624,
 	"th13/face/enemy6/face06bs.png": 530549061,
 	"th13/face/enemy6/face06ct.png": 4181796436,
 	"th13/stgenm/stage06/enm6.png": 3628514508,
--- repo.js
+++ repo.js
@@ -3,7 +3,7 @@
 	"id": "GhostPhanom",
 	"patches": {
 		"BossRush": "Turbo youkai extermination",
-		"MikoIcecream": "Changes her shaku and aura for a Cirno icecream"
+		"MikoIcecream": "Changes her shaku and aura for a Cirno icecream in TH13"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/GhostPhanom/GhostPhanom-Thcrap/master/"
