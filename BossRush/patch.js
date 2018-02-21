57353b25ab2bc4c69c88e1a90857ca260d36e6ff
GhostPhanom <34610185+GhostPhanom@users.noreply.github.com>
2018-02-22 00:35:08 +0100

    TH16

--- BossRush/patch.js
+++ BossRush/patch.js
@@ -7,5 +7,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/GhostPhanom/GhostPhanom-Thcrap/master/BossRush/"
 	],
-	"title": "Turbo youkai extermination (Currently supports TH10, TH11, TH12, 128Ex, 13, 14, 15)"
+	"title": "Turbo youkai extermination (Currently supports TH10, TH11, TH12, 128Ex, 13, 14, 15, 16)"
 }
