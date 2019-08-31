f54cc45e33bf5bc7a1ebc2267b5636a6134b3783
GhostPhanom <theghostphanom@gmail.com>
2019-08-31 14:42:12 +0200

    RAWR

--- MikoPopsicle/files.js
+++ MikoPopsicle/files.js
@@ -3,5 +3,10 @@
 	"th13/face/enemy6/face06bs.png": 530549061,
 	"th13/face/enemy6/face06ct.png": 4181796436,
 	"th13/stgenm/stage06/enm6.png": 3628514508,
-	"th13/stgenm/stage06/enm6b.png": 2410803754
+	"th13/stgenm/stage06/enm6b.png": 2410803754,
+	"th17/bullet/beast1.png": 1804546833,
+	"th17/bullet/beastf.png": 318370936,
+	"th17/front/front00.png": 2913692541,
+	"th17/title/weapon00.png": 2391760138,
+	"th17/title/weapon01.png": 921926898
 }
