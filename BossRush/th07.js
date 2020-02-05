7535160129e65d6d18eb38215cb79dab07782882
rosenrose <crazytempler@gmail.com>
2020-02-05 12:12:35 +0900

    th07 bossrush with middle boss

---
+++ BossRush/th07.js
@@ -0,0 +1,82 @@
+{
+	"binhacks": {
+		"jump1": {
+			"addr": "0x0041FD21",
+			"code": "E9 5AD10600"
+		},
+		"jump2": {
+			"addr": ["0x00429020","0x00429091","0x00429102","0x00429173",
+				"0x004291E4","0x00429255","0x004292C6","0x00429334"],
+			"code": "E9 1E000000"
+		},
+		"hack1": {
+			"addr": "0x0042F02B",
+			"code": "EB16"
+		},
+		"hack2": {
+			"addr": "0x0048CE80",
+			"code": "50 A1 5CF86200 6BC0 20"
+		},
+		"group1_1": {
+			"addr": "0x0048CE89",
+			"code": "8B88 E8CE4800 3B0D FCE02F01 7520"
+		},
+		"group1_2": {
+			"addr": "0x0048CE97",
+			"code": "8B88 ECCE4800 890D FCE02F01 890D 0CE12F01 8B88 E4CE4800 890D A4F86200 EB3C"
+		},
+		"group1_3": {
+			"addr": "0x0048CEB7",
+			"code": "8B88 F0CE4800 3B0D FCE02F01 7514"
+		},
+		"group1_4": {
+			"addr": "0x0048CEC5",
+			"code": "8B88 F4CE4800 890D FCE02F01 890D 0CE12F01 EB1A"
+		},
+		"group1_5": {
+			"addr": "0x0048CED9",
+			"code": "8B88 F8CE4800 3B0D FCE02F01 750C"
+		},
+		"group1_6": {
+			"addr": "0x0048CEE7",
+			"code": "8B88 FCCE4800 890D FCE02F01"
+		},
+		"hack3": {
+			"addr": "0x0048CEF3",
+			"code": "58 B9 50595700 E9 282EF9FF"
+		},
+		"group2_1": {
+			"addr": "0x0048CF00",
+			"code": "28000000 20000000 B4000000 400A0000 180F0000 40130000 FFFFFF7F FFFFFF7F"
+		},
+		"group2_2": {
+			"addr": "0x0048CF20",
+			"code": "80000000 20000000 3C000000 000B0000 80180000 C01D0000 FFFFFF7F FFFFFF7F"
+		},
+		"group2_3": {
+			"addr": "0x0048CF40",
+			"code": "80000000 20000000 3C000000 40030000 80030000 00070000 48070000 400D0000"
+		},
+		"group2_4": {
+			"addr": "0x0048CF60",
+			"code": "80000000 20000000 B4000000 5A1B0000 8C270000 80390000 FFFFFF7F FFFFFF7F"
+		},
+		"group2_5": {
+			"addr": "0x0048CF80",
+			"code": "80000000 20000000 3C000000 C0120000 40130000 C0170000 FFFFFF7F FFFFFF7F"
+		},
+		"group2_6": {
+			"addr": "0x0048CFA0",
+			"code": "80000000 20000000 3C000000 40070000 80070000 C0090000 FFFFFF7F FFFFFF7F"
+		},
+		"group2_7": {
+			"addr": "0x0048CFC0",
+			"code": "80000000 20000000 3C000000 C00F0000 F00F0000 40220000 FFFFFF7F FFFFFF7F"
+		},
+		"group2_8": {
+			"addr": "0x0048CFE0",
+			"code": "80000000 20000000 3C000000 400F0000 900F0000 00240000 FFFFFF7F FFFFFF7F"
+		}
+	},
+	"title": "TH07 Boss Rush"
+}
